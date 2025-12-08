8d146263c7f5332782ccf912d01f2a52bc84f449
aylinjosephine <aylinkutluk@web.de>
2025-12-08 23:14:34 +0100

    Just Comments

--- src/AppRoot.jsx
+++ src/AppRoot.jsx
@@ -1,3 +1,7 @@
+/* Hier habe ich testweise versucht, nicht über die Main zu gehen und dafür ein
+anderes file erstellt, um trotzdem theoretisch über die (alte) index.html (nun index-standalone.html)
+das submodule selbstständig zu erhalten. die datei hier wird nun statt der main.jsx
+im fsmwrapper im hauptprojekt geladen */
 import { StrictMode } from "react";
 import { Provider } from "jotai";
 import { App } from "./app.jsx";
