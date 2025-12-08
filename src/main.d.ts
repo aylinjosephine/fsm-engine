5a9a894d456cf768d415ab7a68edded927666e8a
aylinjosephine <aylinkutluk@web.de>
2025-12-08 21:37:52 +0100

    "Fixed" Config Files

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,7 +1,2 @@
-// src/types/main.d.ts
-declare module '../../fsm-submodule/src/main.jsx' {
-  import React from 'react'
-
-  const MainApp: React.FC | React.ReactElement
-  export default MainApp
-}
+const Main: unknown;
+export default Main;
